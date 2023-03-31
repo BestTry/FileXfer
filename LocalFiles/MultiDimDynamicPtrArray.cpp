3a9d561b0a5fa93c1698f722eeecf3e66a0cb014
Tom Ricci <lordbless77@hotmail.com>
2023-03-30 19:22:52 -0600

    Started code for multiDynArrayPtrs

---
+++ LocalFiles/MultiDimDynamicPtrArray.cpp
@@ -0,0 +1,20 @@
+// MultiDimDynamicPtrArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Code demonstrates an example of a two dimensional dynamic array using pointers.  
+// The array is dynamic and therefore will be using the keywords 'new' amd delete for memory allocation and de-allocation
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Multi Dimensional Dynamic Array With Pointers\n" << endl;
+
+
+
+
+
+    system("pause>0");
+    return 0;
+}
