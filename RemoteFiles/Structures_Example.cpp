a8712cff6c4bfb07356c36d7a60ec768f3be0602
Tom Ricci <lordbless77@hotmail.com>
2023-04-04 15:40:03 -0600

    Updated structs example code

--- RemoteFiles/Structures_Example.cpp
+++ RemoteFiles/Structures_Example.cpp
@@ -10,84 +10,82 @@ using std::string;
 
 // ............................................................................................................................... /
 
-struct Vehicle          // This struct has to be defined BEFORE person  struct cause person objecs must know about vehicles
+struct Vehicle          // This Vehicle struct has to be defined BEFORE person struct cause person objecs must know about vehicles
 {                       // Attributes - Properties of Vehicle
-   string make;
-   string color;
-   int year;
-   int mpg;
-   string type;         // Truck Car Bicycle
+    string make;
+    string color;
+    int year;
+    int mpg;
+    string type;         // Truck Car Bicycle SUV 
 
-   void DisplayVehicleData()
-   {
-      cout << " Vehicles Data: " << endl;
-      cout << " Type: " << type << endl;
-      cout << " Make: " << make << endl;
-      cout << " Year: " << year << endl;
-      cout << " Color: " << color << endl;
-      cout << " MPG: " << mpg << endl;
-   }
+    void DisplayVehicleData()
+    {
+        cout << " Vehicles Data: " << endl;
+        cout << " Type: " << type << endl;
+        cout << " Make: " << make << endl;
+        cout << " Year: " << year << endl;
+        cout << " Color: " << color << endl;
+        cout << " MPG: " << mpg << endl;
+    }
 };                      // Semicolon is required here!
 
 struct Person
 {
-   int age;             // Attributes - Properties of Person
-   string firstName;
-   string  lastName;
-   int weight;
-   Vehicle vehicle;    // Within the person struct include a Vehicle attribute.
+    int age;            // Attributes - Properties of Person
+    string firstName;
+    string  lastName;
+    int weight;
+    Vehicle vehicle;    // Within the person struct include a Vehicle attribute.
 
-   void DisplayPersonData()
-   {
-      cout << " Person Data: " << endl;
-      cout << " First Name: " << firstName << endl;
-      cout << " LastName: " << lastName << endl;
-      cout << " weight: " << weight << endl;
+    void DisplayPersonData()
+    {
+        cout << " Person Data: " << endl;
+        cout << " First Name: " << firstName << endl;
+        cout << " LastName: " << lastName << endl;
+        cout << " weight: " << weight << endl;
 
-   }
-};
+    }
+};                      // Semicolon is required here! - Struct or classes
 
 
-
+                        // Independenet Function - Not part of any Struct - Takes Vehicle and Person structs as arguments
 void DisplayAllData(Vehicle vehicle, Person person)
-{
-   vehicle.DisplayVehicleData();
-   cout << "\n\n " << endl;
-   person.DisplayPersonData();
+{   
+    person.DisplayPersonData();
+    cout << endl;
+    vehicle.DisplayVehicleData();
 }
 
-// ................................................................................................................... 
+// ....................................................   Main Program    .................................................................. /
 
 
-int main()              
+int main()
 {
-   cout << " Structures Example\n" << endl;
+    cout << " Structures Example\n" << endl;
+                            // This struct does not have a parametized constructor: Assign params this way
+    Vehicle fourRunner;     // This struct instantiation has to be defined BEFORE person tom has to know the vehicle object
+    fourRunner.make = "Toyota";
+    fourRunner.color = "Dark Green";
+    fourRunner.year = 1997;
+    fourRunner.mpg = 19;
+    fourRunner.type = "SUV";
 
-   Vehicle fourRunner;   // This struct instantiation has to be defined BEFORE person tom has to know the vehicle object
-   fourRunner.make = "Toyota";
-   fourRunner.color = "Dark Green";
-   fourRunner.year = 1997;
-   fourRunner.mpg = 19;
-   fourRunner.type = "SUV";
+                            // Instantiated Person object 
+    Person tom;             // This struct does not have a parametized constructor: Assign params this way
+    tom.firstName = "Thomas";
+    tom.lastName = "Ricci";
+    tom.weight = 167;
+    tom.age = 75;
+    tom.vehicle = fourRunner;
+                            // Function call
+    tom.DisplayPersonData();
 
+    cout << endl;
+                            // Function call
+    fourRunner.DisplayVehicleData();
 
-   Person tom;          // This struct does not have a parametized constructor: Assign params this way
-   tom.firstName = "Thomas";
-   tom.lastName = "Ricci";
-   tom.weight = 167;
-   tom.age = 75; 
-   tom.vehicle = fourRunner;
-   
-
-
-   tom.DisplayPersonData();
-
-   cout << "\n\n " << endl;   
-
-   fourRunner.DisplayVehicleData();
-
-   cout << "\n\n Displaying all data with one function call: " << endl;
-
-   DisplayAllData(fourRunner, tom);
+    cout << "\n\n Displaying all strucs data with one function call: " << endl;
+                            // Display all struct properties for two instantiations
+    DisplayAllData(fourRunner, tom);
 }
 
